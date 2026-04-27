7f82648f0aed7af1de91822030e20c9282e4a16e
akshit <108526799+Rizzy-prgm@users.noreply.github.com>
2026-04-27 16:29:12 +0530

    Day3Program_4

---
+++ Day3Program_4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+ int main () {
+     
+     char ch = '1';
+     int num = 1;
+     
+     cout << endl;
+     switch( ch ) { 
+         
+        case 1: cout << "First" <<endl;
+                cout << "First again" <<endl;
+                break;
+                
+        case '1': switch(num) { 
+        case 1: cout<<"value of num is"<< num <<endl;
+        break;
+        }
+        break;
+        
+        default: cout << "It is default case" <<endl;
+        
+ }
+ 
+ cout << endl;
+ 
+ return 0;
+ }
