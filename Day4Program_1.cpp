2e5d9a56982a67684a413396a48e6cf70d121845
akshit <108526799+Rizzy-prgm@users.noreply.github.com>
2026-04-28 16:09:52 +0530

    Day4Program_1
    Initialize Day4Program_1.cpp with basic structure.

---
+++ Day4Program_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// HOLD
+void printArray() {
+    
+}
+
+   int main () {
+       
+       //declare 
+       int number [15];
+       
+    cout << "value at 15 index " << number[14] << endl;
+    
+cout << endl << " Everything is Fine " << endl << endl;
+
+    return 0;
+}
